1f1cbf36487c49e7cfe00d733a15cd94a32c3880
Oleksandr Lebediev <san.lebed1998@gmail.com>
2022-10-04 21:32:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ export const App = () => {
       <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
       <Statistic data={data} />
       <FriedList friends={friends} />
+
       <TransactionHistory transactions={transactions} />
     </div>
   );
